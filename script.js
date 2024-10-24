02e531656d90c73e6f85522336427c0b1442be25
akshay53156 <akshay53156@gmail.com>
2024-10-25 02:40:01 +0530

    added basic features

---
+++ script.js
@@ -0,0 +1,43 @@
+let button = document.getElementById("button");
+let textArea = document.getElementById("note-input");
+let container = document.querySelector(".container");
+let color = document.getElementById("color");
+let noNotes = document.getElementById("noNotes");
+
+button.addEventListener("click", createNote);
+
+function createNote(){
+    
+    if( textArea.value === ""){
+        alert("Enter some Text");
+        return;
+    }
+    let notesContainer = document.createElement("div");
+    notesContainer.classList.add("notesContainer");
+    let p = document.createElement("p");
+    let btn = document.createElement("button");
+    btn.classList.add("crossBtn");
+
+    notesContainer.appendChild(p);
+    notesContainer.appendChild(btn);
+    notesContainer.style.backgroundColor = color.value;
+    p.innerText = textArea.value;
+    btn.innerText = "X";   
+
+    container.appendChild(notesContainer);
+
+    noNotes.style.display = "none";
+
+    btn.addEventListener("click", () => {
+        notesContainer.remove();
+        let notesSet = document.querySelectorAll(".notesContainer");
+        if(notesSet.length === 0){
+            noNotes.style.display = "block";
+        }
+    });
+
+    textArea.value = "";
+};
+
+
+
